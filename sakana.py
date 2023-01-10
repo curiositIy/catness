d27cb9840816bcf788e7f252f3fc7405d9835d05
curiositly <curiositlyxd@gmail.com>
2023-01-10 14:24:49 -0900

    Add config example

--- sakana.py
+++ sakana.py
@@ -3,13 +3,6 @@
 import logging.handlers
 import discord
 
-## API Keys :333
-TOKEN = "the token in question"
-LASTFM = "the api key goes here"
-STEAM = "read above"
-OPENAI = "same"
-TENOR = "the horrific tales of zingus"
-
-prefix = '.'
+prefix = "."
 
 intents = discord.Intents.default()
